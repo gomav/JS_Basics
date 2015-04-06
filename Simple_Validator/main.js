94273414594780861925458246d9397ae7b4adda
Manny Navarro <manny.navarro@gmail.com>
2015-04-06 17:01:57 -0600

    Re-activated previously commented-out code

--- Simple_Validator/main.js
+++ Simple_Validator/main.js
@@ -1,9 +1,9 @@
-// var input = prompt("What is your phone number (Format: xxx-xxx-xxxx)?");
-// alert(input.charAt(3) === '-' && input.charAt(7) === '-');
-// var input = prompt("What is your birth date (Format: xx/xx/xx)?");
-// alert(input.charAt(2) === '/' && input.charAt(5) === '/');
-// var input = prompt("What is your postal code (Format: xxxxx)?");
-// alert(input.length === 5);
+var input = prompt("What is your phone number (Format: xxx-xxx-xxxx)?");
+alert(input.charAt(3) === '-' && input.charAt(7) === '-');
+var input = prompt("What is your birth date (Format: xx/xx/xx)?");
+alert(input.charAt(2) === '/' && input.charAt(5) === '/');
+var input = prompt("What is your postal code (Format: xxxxx)?");
+alert(input.length === 5);
 var input = prompt("Are you married (Format: yes or no)?");
 if(input.toUpperCase() === "YES") {
   alert("Thank you!");
