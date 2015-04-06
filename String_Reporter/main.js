d706d44e6246c225a4a60eed0a5cdd8115c78870
Manny Navarro <manny.navarro@gmail.com>
2015-04-06 15:02:05 -0600

    program functioning as required

---
+++ String_Reporter/main.js
@@ -0,0 +1,8 @@
+var input = prompt("What is your name?");
+alert(input);
+alert("Length of your name: " + input.length);
+alert("The third letter in your name is " + input[2]);
+alert(input + " in lowercase: " + input.toLowerCase());
+alert(input + " in uppercase: " + input.toUpperCase());
+alert(input + " will be going home exactly at 6pm.");
+alert("Subword from 2nd to the 4th character is: " + input.substring(2, 4));
