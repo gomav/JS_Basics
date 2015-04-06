80d61b7c3ebaa92a94977903ca99959d1caead6d
Manny Navarro <manny.navarro@gmail.com>
2015-04-06 13:59:38 -0600

    code complete & functional

---
+++ Loop_Pratice_1/main.js
@@ -0,0 +1,40 @@
+/*increments of 100*/
+for(var i = 0; i < 501; i+=100){
+  console.log(i);
+}
+
+/*multiplying time 2*/
+for(var i = 1; i < 65; i*=2){
+  console.log(i);
+}
+
+/*solving for 111 222 333 sequence */
+for(var i = 1; i < 4; i+=1){
+  for(var x = 1; x < 4; x+=1){
+    console.log(i);
+  }
+
+  // console.log(i);
+}
+
+/*looping for increments of 2 */
+for(var i = 0; i < 11; i+=2){
+  console.log(i);
+}
+
+/*looping for increments of 3 */
+for(var i = 3; i < 18; i+=3){
+  console.log(i);
+}
+
+/*looping while subtracting by 1 each iteration */
+for(var i = 9; i >= 0; i-=1){
+  console.log(i);
+}
+
+/*repeating 0123 sequence three times */
+for(var i = 0, r=3; i < 4; i+=1){
+  for(var x = 0; x < 4; x+=1){
+  console.log(x);
+  }
+}
