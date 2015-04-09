65ab3c56117850a00467646c32e25e4e11d3e8c3
Manny Navarro <manny.navarro@gmail.com>
2015-04-09 17:47:55 -0600

    Part II of Intro to jQuery functional

--- jQuery_intro/main.js
+++ jQuery_intro/main.js
@@ -2,15 +2,30 @@
 // document is whaterver you are targeting - i.e., button,
 // $(document).on('ready', function() {;
 $('#my-button').on('click', function(){
-  $('body').append('<p>Behavior changed!</p>')
+  $('.part1').append('<p>Behavior changed!</p>')
     console.log('You clicked the button!');
 });
 
 $('#my-button1').on('click', function(){
-  $('body').append('<h1>This is h1 button</h1>')
+  $('.part1').append('<h1>This is h1 button</h1>')
 });
 
 $('#my-button2').on('click', function(){
 
-  $('body').append('<ul>My list<br></br><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>')
+  $('.part1').append('<ul>My list<br></br><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>')
+});
+
+$('#pink').mouseover(function(){
+  $('#pink').css('color', 'pink');
+
+});
+
+$('#exclamation').append("!");
+
+$('#link').click(function(e){
+    if(!confirm("Are you sure you want to navigate to google.com?")){
+    $(this).remove();
+
+    e.preventDefault();
+  };
 });
