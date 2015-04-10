d71484dd7cc9e8e2538dc2dbd26d82d89b8ab0a3
Manny Navarro <manny.navarro@gmail.com>
2015-04-10 15:45:32 -0600

    updated formatting

--- jQuery_intro/main.js
+++ jQuery_intro/main.js
@@ -1,17 +1,17 @@
 //Part I
 
 $('#my-button').on('click', function(){
-  $('.part1').append('<p>Behavior changed!</p>')
-    console.log('You clicked the button!');
+  $('.part1').append('<p>Behavior changed!</p>');
+  console.log('You clicked the button!');
 });
 
 $('#my-button1').on('click', function(){
-  $('.part1').append('<h1>This is h1 button</h1>')
+  $('.part1').append('<h1>This is h1 button</h1>');
 });
 
 $('#my-button2').on('click', function(){
 
-  $('.part1').append('<ul>My list<br></br><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>')
+  $('.part1').append('<ul>My list<br></br><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>');
 });
 
 //Part II
@@ -24,11 +24,11 @@ $('#pink').mouseover(function(){
 $('#exclamation').append("!");
 
 $('#link').click(function(e){
-    if(!confirm("Are you sure you want to navigate to google.com?")){
-    $(this).remove();
+  if(!confirm("Are you sure you want to navigate to google.com?")){
+  $(this).remove();
 
-    e.preventDefault();
-  };
+  e.preventDefault();
+  }
 });
 
 //Part III
