27011993b83b90c111e343e267aba0c9884a9d26
Manny Navarro <manny.navarro@gmail.com>
2015-04-10 13:17:01 -0600

    Part III of Intro to jQuery is functional

--- jQuery_intro/main.js
+++ jQuery_intro/main.js
@@ -29,3 +29,20 @@ $('#link').click(function(e){
     e.preventDefault();
   };
 });
+
+//Part III
+// $('#my-buttonlg').click(function(e){
+//   $("body").append("<h1>hello world!</h1>")
+// });
+$(function(){
+  console.log("ready!");
+  $("#middle-button").on('click', function() {
+    var html = "<div class = 'popup'><div class='close'>X</div><h1>Button<br>click!!!</h1></div>";
+
+    $("#popup").append(html);
+
+    $(".close").on('click', function() {
+      $("#popup").remove();
+    });
+  });
+});
